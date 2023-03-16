51f8541af90d16fb181dbf570326288d12d279dc
Couls <coul422@gmail.com>
2023-03-16 12:16:40 -0400

    0.7.3

--- src/module/Collision.js
+++ src/module/Collision.js
@@ -40,7 +40,7 @@ export default class Collision {
     const tokenX1 = initTokenPos.x + (tokenCanvasWidth / 2);
     const tokenY1 = initTokenPos.y + (tokenCanvasHeight / 2);
     // Allow movement out of the tile without triggering if you start your movement inside the tile
-    if (initTokenPos.x === tileX1 && initTokenPos.y === tileY1 && tile.flags['hey-wait']?.unlimited) {
+    if (tokenX1 >= tileX1 && tokenX1 <= tileX2 && tokenY1 >= tileY1 && tokenY1 <= tileY2 && tile.flags['hey-wait']?.unlimited) {
       return false;
     }
     const tokenX2 = token.document.x + (tokenCanvasWidth / 2);
@@ -61,29 +61,86 @@ export default class Collision {
     // 3. Create four intersection checks, one for each line making up the
     // tile rectangle. If any of these pass, that means it has intersected at
     // some point.
-    return Boolean(tokenRay.intersectSegment([
+    const intersectionPoints = [];
+
+    if (tokenRay.intersectSegment([
       tileX1,
       tileY1,
       tileX2,
       tileY1,
-    ]))
-      || Boolean(tokenRay.intersectSegment([
+    ])) {
+      intersectionPoints.push(tokenRay.intersectSegment([
+        tileX1,
+        tileY1,
+        tileX2,
+        tileY1,
+      ]));
+    }
+    if (tokenRay.intersectSegment([
+      tileX2,
+      tileY1,
+      tileX2,
+      tileY2,
+    ])) {
+      intersectionPoints.push(tokenRay.intersectSegment([
         tileX2,
         tileY1,
         tileX2,
         tileY2,
-      ]))
-      || Boolean(tokenRay.intersectSegment([
+      ]));
+    }
+    if (tokenRay.intersectSegment([
+      tileX2,
+      tileY2,
+      tileX1,
+      tileY2,
+    ])) {
+      intersectionPoints.push(tokenRay.intersectSegment([
         tileX2,
         tileY2,
         tileX1,
         tileY2,
-      ]))
-      || Boolean(tokenRay.intersectSegment([
+      ]));
+    }
+    if (tokenRay.intersectSegment([
+      tileX1,
+      tileY2,
+      tileX1,
+      tileY1,
+    ])) {
+      intersectionPoints.push(tokenRay.intersectSegment([
         tileX1,
         tileY2,
         tileX1,
         tileY1,
       ]));
+    }
+    if (!intersectionPoints.length) return false;
+    // 4. Find the intersection point nearest to the token's initial position.
+    let nearestIntersectionPoint = null;
+    let nearestIntersectionDistanceSquared = Infinity;
+    for (const intersectionPoint of intersectionPoints) {
+      const distanceSquared = (intersectionPoint.x - tokenX1) ** 2
+      + (intersectionPoint.y - tokenY1) ** 2;
+      if (distanceSquared < nearestIntersectionDistanceSquared) {
+        nearestIntersectionPoint = intersectionPoint;
+        nearestIntersectionDistanceSquared = distanceSquared;
+      }
+    }
+    // if we start right of the tile, nudge final position left
+    if (tokenX1 > tileX2) {
+      nearestIntersectionPoint.x -= global.canvas.grid.size;
+    }
+    // if we start below the tile, nudge final position up
+    if (tokenY1 > tileY2) {
+      nearestIntersectionPoint.y -= global.canvas.grid.size;
+    }
+    // snap to nearest point near intersection
+    const finalintersectionpoint = global.canvas.grid.getSnappedPosition(
+      nearestIntersectionPoint.x, nearestIntersectionPoint.y,
+    );
+    token.document.update({ x: finalintersectionpoint.x, y: finalintersectionpoint.y });
+
+    return true;
   }
 }
